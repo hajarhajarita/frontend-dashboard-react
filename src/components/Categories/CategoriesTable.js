62d27272371e41c77fd44a069f505fd98e0c0901
hajarhajarita <106028645+hajarhajarita@users.noreply.github.com>
2022-05-23 23:43:14 +0100

    Initial commit

---
+++ src/components/Categories/CategoriesTable.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CategoriesTable = () => {
+  return (
+    <div className="col-md-12 col-lg-8">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th className="text-end">Action</th>
+          </tr>
+        </thead>
+        {/* Table Data */}
+        <tbody>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>1</td>
+            <td>
+              <b>amande</b>
+            </td>
+            <td>Amande, produit cosmetique d'amandes</td>
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="/amande"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>2</td>
+            <td>
+              <b>datte</b>
+            </td>
+            <td>tous les types des dattes marocaines</td>
+
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="/datte"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>3</td>
+            <td>
+              <b>safran</b>
+            </td>
+            <td>l'or rouge du maroc safran</td>
+
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="/safran"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+
+          </tr>
+          <tr>
+            <td>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" />
+              </div>
+            </td>
+            <td>4</td>
+            <td>
+              <b>rose</b>
+            </td>
+            <td>les rose et les produits cosmetiques de base du rose</td>
+            <td className="text-end">
+              <div className="dropdown">
+                <Link
+                  to="/rose"
+                  data-bs-toggle="dropdown"
+                  className="btn btn-light"
+                >
+                  <i className="fas fa-ellipsis-h"></i>
+                </Link>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="#">
+                    Edit info
+                  </Link>
+                  <Link className="dropdown-item text-danger" to="#">
+                    Delete
+                  </Link>
+                </div>
+              </div>
+            </td>
+            
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CategoriesTable;
---
+++ src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "safran",
+    image: "/images/6.png",
+    description:
+      "Le safran est une épice riche de calories si vous en consommez 100 g (352 Cal/100 g) ; or, la consommation est de l’ordre de la pincée donc à relativiser. De même, il est riche en minéraux, en oligo-éléments et en vitamines…pour 100 grammes",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "amande",
+    image: "/images/5.png",
+    description:
+      "amandes blanchies, se distinguent des amandes brutes par l'absence de peau brune. En bouche, les amandes émondées auront une saveur plus douce que les amandes brutes.",
+    price: 59,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "datte ",
+    image: "/images/4.png",
+    description:
+      "une datte molle et sucrée, de grande taille, pe- sant entre 20 et 30 grammes, tandis que les autres dattes varient de 6 à 11 grammes. La récolte commence fin juillet-début août et se termine en octobre-novembre",
+    price: 20,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "huile d'amande",
+    image: "/images/3.png",
+    description:
+      "L'huile d'amande douce hydrate et protège la peau et les cheveux, tout en leur redonnant de la vigueur et de l'éclat. Apaisante, elle soulage les irritations et les démangeaisons. Nourrissante, elle est parfaite en cas de gerçures ou de peaux desséchées, comme en prévention des vergetures",
+    price: 39,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "safran",
+    image: "/images/2.png",
+    description:
+      "Le safran est le nom que l'on donne à l'épice produit par la plante nommée Crocus Sativus L. Cette épice vient du prélèvement et du séchage des trois stigmates écarlates de Crocus Sativus L. On peut souvent retrouver ce dernier sous le nom de crocus de safranoignon de safran",
+    price: 39,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "safran",
+    image: "/images/1.png",
+    description:
+      "Le safran est le nom que l'on donne à l'épice produit par la plante nommée Crocus Sativus L. Cette épice vient du prélèvement et du séchage des trois stigmates écarlates de Crocus Sativus L. On peut souvent retrouver ce dernier sous le nom de crocus de safranoignon de safran",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
